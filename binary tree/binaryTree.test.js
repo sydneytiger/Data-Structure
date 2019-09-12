484a789d0976cc85b154c35b299bd6635155cf51
Tiger Chen <tigerchen@Tigers-MBP-2.gateway>
2019-09-12 22:56:30 +1000

    added binary tree functions and tests

--- binary tree/binaryTree.test.js
+++ binary tree/binaryTree.test.js
@@ -1,14 +1,104 @@
 const Tree = require('./index');
 
 describe('Binary tree', () =>{
-    let tree;
-    beforeEach(() => {
-        tree = new Tree();
+  let tree;
+  let emptyTree;
+  let copyTree;
+  beforeAll(() => {
+    tree = new Tree();
+    tree.insert(7);
+    tree.insert(4);
+    tree.insert(9);
+    tree.insert(1);
+    tree.insert(6);
+    tree.insert(8);
+    tree.insert(11);
+    tree.insert(2);
+
+    emptyTree = new Tree();
+  });
+
+  describe('test contains', () => {
+    it('should empty tree not contains value 7', () => {
+      expect(emptyTree.contains(7)).toBeFalsy();
     });
 
-    it('should return min', () => {
-        expect(tree.min()).toEqual(1);
+    it('should tree contains value 7', () => {
+      expect(tree.contains(7)).toBeTruthy();
     });
+  
+    it('should tree contains value 4', () => {
+      expect(tree.contains(4)).toBeTruthy();
+    });
+  
+    it('should tree contains value 9', () => {
+      expect(tree.contains(9)).toBeTruthy();
+    });
+  
+    it('should tree contains value 1', () => {
+      expect(tree.contains(1)).toBeTruthy();
+    });
+  
+    it('should tree contains value 6', () => {
+      expect(tree.contains(6)).toBeTruthy();
+    });
+
+    it('should tree contains value 8', () => {
+      expect(tree.contains(8)).toBeTruthy();
+    });
+
+    it('should tree contains value 11', () => {
+      expect(tree.contains(11)).toBeTruthy();
+    });
+
+    it('should tree contains value 2', () => {
+      expect(tree.contains(2)).toBeTruthy();
+    });
+
+    it('should tree not contains value 12', () => {
+      expect(tree.contains(12)).toBeFalsy();
+    });
+  });
+
+  describe('test min', () => {
+    it('should tree min return 2', () => {
+      expect(tree.min()).toEqual(2);
+    });
+
+    it('should empty tree min return Number.MAX_VALUE', () => {
+      expect(emptyTree.min()).toEqual(Number.MAX_VALUE);
+    });
+  });
+
+  describe('test max', () => {
+    it('should tree max return 11', () => {
+      expect(tree.max()).toEqual(11);
+    });
+
+    it('should empty tree max return Number.MAX_VALUE', () => {
+      expect(emptyTree.max()).toEqual(Number.MIN_VALUE);
+    });
+  });
+
+  describe('test size', () => {
+    it('should tree have size of 8', () => {
+      expect(tree.size()).toEqual(8);
+    });
+
+    it('should empty tree have size of 0', () => {
+      expect(emptyTree.size()).toEqual(0);
+    });
+  });
+
+  describe('test countLeaf', () =>{
+    it('should tree have leaf of 4', () => {
+      expect(tree.countLeaf()).toEqual(4);
+    });
+
+    it('should empty tree have leaf of 0', () => {
+      expect(emptyTree.countLeaf()).toEqual(0);
+    });
+  });
 });
 
 
--- binary tree/index.js
+++ binary tree/index.js
@@ -11,9 +11,46 @@ class Tree {
     this.root = null;
   }
   
-  insert(value){};
+  insert(value){
+    const newNode = new Node(value);
+    if(!this.root){
+      this.root = newNode;
+      return ;
+    }
+
+    let current = this.root;
+    while(true){
+      if(value > current.value){
+        if(!current.right){
+          current.right = newNode;
+          return;
+        }
+        current = current.right;
+      }
+      else{
+        if(value < current.value){
+          if(!current.left){
+            current.left = newNode;
+            return;
+          }
+          current = current.left;
+        }
+      }
+    }
+  };
 
   find(value){};
+
+  contains(value){
+    const search = (node, value) => {
+      if(!node) return false;
+      return node.value === value 
+        || search(node.left, value) 
+        || search(node.right, value);
+    }
+
+    return search(this.root, value);
+  }
 
   // pre-order traversal: root left right
   preorderTravesal(node, arr){};
@@ -24,17 +61,51 @@ class Tree {
   // post-order traversal: left right root
   postorderTravesal(node, arr){};
 
-  isLeaf(node){};
+  isLeaf(node){
+    return !node.left && !node.right;
+  };
 
   height(){};
 
   min(){
-      return 1;
+    const findMin = node => {
+      if(!node) return Number.MAX_VALUE;
+      if(this.isLeaf(node)) return node.value;
+      return Math.min(findMin(node.left), findMin(node.right));
+    }
+    return findMin(this.root);
   };
 
-  max(){
-      return 2;
+  max(node){
+    const findMax = node => {
+      if(!node) return Number.MIN_VALUE;
+      if(this.isLeaf(node)) return node.value;
+      return Math.max(findMax(node.left), findMax(node.right));
+    }
+    return findMax(this.root);
   };
+
+  size(){
+    const getSize = (node, total) => {
+      if(!node) return;
+      total.count++;
+      return getSize(node.left, total) + getSize(node.right, total);
+    }
+
+    const total = { count: 0};
+    getSize(this.root, total);
+    return total.count;
+  }
+
+  countLeaf(){
+    const getLeaf = node => {
+      if(!node) return 0;
+      if(this.isLeaf(node)) return 1;
+      return getLeaf(node.left) + getLeaf(node.right);
+    }
+
+    return getLeaf(this.root);
+  }
 }
 
 module.exports = Tree;
